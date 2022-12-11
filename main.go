f1fbe7836943e97946bec7b861827acc205bff8c
Max <max-frisch@gmx.net>
2022-12-11 20:12:02 +0700

    rock, paper, scissors web

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/max-frisch/goRPSGame/rps"
+)
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
 
+func playRound(w http.ResponseWriter, r *http.Request) {
+	winner, computerChoice, roundResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roundResult)
 }
 
 func main() {
+	http.HandleFunc("/play", playRound)
+	http.HandleFunc("/", homePage)
 
+	log.Println("Starting web server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
